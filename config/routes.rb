48303bb666b4e84985c3921e4332fc7c4d314510
Divij <divij.mehra.16@gmail.com>
2018-10-11 14:43:46 +1100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get 'pages/thoughts'
 
-  get 'pages/thoughts/whywrite'
+  resources :posts 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
